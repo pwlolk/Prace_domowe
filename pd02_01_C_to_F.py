36b181b3afff0223c5381c56c620de16df3ea7f4
pwlolk <p.j.olszak@gmail.com>
2018-11-15 18:36:31 +0100

    Whatever

--- pd02_01_C_to_F.py
+++ pd02_01_C_to_F.py
@@ -1,6 +1,6 @@
 #Zamiana stopni Celsjusza na stopnie Fahrenhaita
 intro = "Zamiana stopni Celsjusza na stopnie Fahrenheita"
-wzor_descr = "Posłużymy się następującym wzorem [ºF]=[ºC]*1.8+32"
+wzor_descr = "Posłużymy się następującym wzorem [°F]=[°C]*1.8+32"
 print("")
 print(intro.upper())
 print(wzor_descr)
@@ -8,8 +8,8 @@ print("")
 temp_C = input("Podaj wartośc temperatury w stopniach Celsjusza: ")
 temp_F = float(temp_C)*1.8+32
 temp_F_2 = format(temp_F, '.2f')
-print("Temperatura w skali Fahrenheita: " + str(temp_F) + "ºF")
-print("Temperatura w skali Fahrenheita, z dokładnością do drugiego miejsca po przecinku: " + str(temp_F_2) + "ºF")
+print("Temperatura w skali Fahrenheita: " + str(temp_F) + "°F")
+print("Temperatura w skali Fahrenheita, z dokładnością do drugiego miejsca po przecinku: " + str(temp_F_2) + "°F")
 print("")
 decyzja = input("Czy chcesz powtórzyć przeliczenie? [T/N]: ")
 decyzja = decyzja.capitalize()
