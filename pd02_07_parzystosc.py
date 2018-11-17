4e61c27fc72669bae4ac4304f3717df14f609b16
pwlolk <p.j.olszak@gmail.com>
2018-11-17 14:31:53 +0100

    Pierwsze podejście

--- pd02_07_parzystosc.py
+++ pd02_07_parzystosc.py
@@ -4,9 +4,9 @@ while True:
     number = input("Podaj liczbę całkowitą: ")
     number_by_2 = (int(number)/2)
     if ((number_by_2) - int(number_by_2) == 0):
-        print("Liczba: " + str(number) + " jest liczbą parzystą")
+        print("Liczba " + str(number) + " jest liczbą parzystą.")
     else:
-        print("Liczba: " + str(number) + " jest liczbą nieparzystą")
+        print("Liczba " + str(number) + " jest liczbą nieparzystą.")
         print ("")
 
 #Sporo zasobów na heheszki użytkowników, np. podanie 0, podanie liczby niecałkowitej, podanie litery itd.
