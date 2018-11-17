33d15f9b427e14a456d9deda2a99839c536e5f1e
pwlolk <p.j.olszak@gmail.com>
2018-11-17 15:15:10 +0100

    Pierwsze podejście

---
+++ pd02_08_3_lub_5_lub_7.py
@@ -0,0 +1,25 @@
+#Sprawdzenie czy liczba jest podzielna przez 3 lub 5 lub 7 + 3 i 5 i 7.
+print("Sprawdzenie czy zadana liczba całkowita jest podzielna przez 3, 5 lub 7.".upper())
+while True:
+    number = input("Podaj liczbę całkowitą: ")
+    number_by_3 = (int(number) / 3)
+    number_by_5 = (int(number) / 5)
+    number_by_7 = (int(number) / 7)
+    if ((number_by_3) - int(number_by_3) == 0):
+        statement_by_3 = "jest"
+    else:
+        statement_by_3 = "nie jest"
+    if ((number_by_5) - int(number_by_5) == 0):
+        statement_by_5 = "jest"
+    else:
+        statement_by_5 = "nie jest"
+    if ((number_by_7) - int(number_by_7) == 0):
+        statement_by_7 = "jest"
+    else:
+        statement_by_7 = "nie jest"
+    if (statement_by_3 == "jest" and statement_by_5 == "jest" and statement_by_7 == "jest"):
+        print("Liczba " + str(number) + " jest podzielna przez 3, 5 i 7")
+    else:
+        print("Liczba " + str(number) + " " + str(statement_by_3) + " podzielna przez 3, " + str(statement_by_5) + " podzielna przez 5 i " + str(statement_by_7) + " podzielna przez 7.")
+        print ("")
+
