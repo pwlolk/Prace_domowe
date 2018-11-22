09bca440f2ca07f8296a3c20b404eb3a12a73ffb
pwlolk <p.j.olszak@gmail.com>
2018-11-22 18:20:51 +0100

    Wersja końcowa

--- PD03/pd03_03_piramida.py
+++ PD03/pd03_03_piramida.py
@@ -2,6 +2,18 @@
 
 print("Rysowanie piramidy z # o zadanej wysokości \n".upper())
 
+rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
+row_current = 1
+
+print('')
+
+while (row_current <= rows_total):
+    print(((2*row_current-1)*'#').center(2*rows_total-1))
+    row_current = row_current + 1
+print("")
+
+#PODEJŚCIA NIEUDANE (NIEDOKŁADNIE WPATRZYŁEM SIĘ W TREŚĆ ZADANIA)
+
 # rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
 # row_current = 1
 # while (row_current <= rows_total):
@@ -9,12 +21,12 @@ print("Rysowanie piramidy z # o zadanej wysokości \n".upper())
 #      row_current = row_current + 1
 # print("")
 
-rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
-row_current = 1
-while (row_current <= rows_total):
-    print((row_current*'#').center(rows_total))
-    row_current = row_current + 1
-print("")
+# rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
+# row_current = 1
+# while (row_current <= rows_total):
+#     print((row_current*'#').center(rows_total))
+#     row_current = row_current + 1
+# print("")
 
 # rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
 # row_current = 1
@@ -26,9 +38,9 @@ print("")
 #     row_current = row_current + 1
 # print("")
 
-rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
-row_current = 1
-while (row_current <= rows_total):
-    print((rows_total-row_current)*' ' + row_current*'#')
-    row_current = row_current + 1
-print("")
+# rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
+# row_current = 1
+# while (row_current <= rows_total):
+#     print((rows_total-row_current)*' ' + row_current*'#')
+#     row_current = row_current + 1
+# print("")
