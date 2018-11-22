3312a5ba66b5c9c28eba7230704c71a918247a5a
pwlolk <p.j.olszak@gmail.com>
2018-11-22 18:05:19 +0100

    Pierwsze niezbyt ładne podejście

---
+++ PD03/pd03_04_pies.py
@@ -0,0 +1,13 @@
+# Wiek psa
+
+print("obliczanie wieku psa \n".upper())
+
+while True:
+    dogs_human_years = int(input("Ile pełnych lat ma pies? "))
+    dogs_human_months = int(input("Ile dodatkowo miesięcy ma pies? "))
+    dogs_human_months_only = dogs_human_years*12+dogs_human_months
+
+    if (dogs_human_months_only <= 24):
+        print('"Prawdziwy" wiek psa to ' + str(format(float(dogs_human_months_only*10.5/12), '.2f')) + ' lat.\n')
+    else:
+        print('"Prawdziwy" wiek psa to ' + str(format(float(2+(dogs_human_months_only*4.5/12)), '.2f')) + ' lat.\n')
