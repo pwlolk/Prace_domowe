5505aadce2ee4cd269f46749c224403fcc989c04
pwlolk <p.j.olszak@gmail.com>
2018-11-22 18:26:55 +0100

    Wersja końcowa

--- PD03/pd03_03_piramida.py
+++ PD03/pd03_03_piramida.py
@@ -4,8 +4,8 @@ print("Rysowanie piramidy z # o zadanej wysokości \n".upper())
 
 rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
 row_current = 1
-
 print('')
+
 while (row_current <= rows_total):
     print(((2*row_current-1)*'#').center(2*rows_total-1))
     row_current = row_current + 1
