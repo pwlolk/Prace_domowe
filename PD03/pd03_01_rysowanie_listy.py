7ce58338bfbde78ca8e206774d2bcacec0c0ee84
pwlolk <p.j.olszak@gmail.com>
2018-11-23 20:21:41 +0100

    Napoczęte

---
+++ PD03/pd03_01_rysowanie_listy.py
@@ -0,0 +1,7 @@
+#Rysowanie prostokąta o zadanej szerokości i wysokości
+print("Rysowanie prostokąta o zadanej szerokości i wysokości".upper())
+lista = [col1, col2, col3]
+
+print(3*("+" + "----" + "+"))
+print((height-2)*("|" + (width-2)*" " + "|" +"\n"), end="")
+print(3*("+" + "----" + "+"))+
