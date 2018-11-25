5dce2eed6eb79858c7b444e00d258af50ec185d7
pwlolk <p.j.olszak@gmail.com>
2018-11-25 10:18:45 +0100

    Koniec (toporny)

--- PD03/pd03_04_pies.py
+++ PD03/pd03_04_pies.py
@@ -8,6 +8,6 @@ while True:
     dogs_human_months_only = dogs_human_years*12+dogs_human_months
 
     if (dogs_human_months_only <= 24):
-        print('"Prawdziwy" wiek psa to ' + str(format(float(dogs_human_months_only*10.5/12), '.2f')) + ' lat.\n')
+        print('"Psi" wiek psa to ' + str(format(float(dogs_human_months_only*10.5/12), '.2f')) + ' lat.\n')
     else:
-        print('"Prawdziwy" wiek psa to ' + str(format(float(21+((dogs_human_months_only-24)*4.5/12)), '.2f')) + ' lat.\n')
+        print('"Psi" wiek psa to ' + str(format(float(21+((dogs_human_months_only-24)*4.5/12)), '.2f')) + ' lat.\n')
