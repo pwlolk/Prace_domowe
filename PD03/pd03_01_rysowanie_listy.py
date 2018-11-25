afb245009a2926c744e637b383ac3c7b119b94ef
pwlolk <p.j.olszak@gmail.com>
2018-11-25 17:57:33 +0100

    Koniec

--- PD03/pd03_01_rysowanie_listy.py
+++ PD03/pd03_01_rysowanie_listy.py
@@ -11,20 +11,19 @@ while (dec.capitalize() != 'N'):
     dec = input('Czy chcesz dodać kolejny element? [dowolony znak - tak / N lub n - nie]')
 
 print('')
-print(list, end ='')
+print(list, end = '')
 print(' --> Szczerze mówiąc, nie rozumiem dlaczego wszystkie elementy w liście lądują jako stringi. \n')
 
 # Szerokość okienka tabeli
-max_el = max(list, key=len)
+max_el = max(list, key = len)
 cell_length = len(max_el) + 2
 cells_total = len(list)
 
 # Rysowanie z wyśrodkowaniem
 index = 0
-length = len(list)
 print('+' + cells_total*((cell_length*'-') + '+'))
 print('+', end = '')
-while(index < length):
+while(index < cells_total):
     print(list[index].center(cell_length) + '+', end = '')
     index = index + 1
 print('\n+' + cells_total*((cell_length*'-') + '+'))
