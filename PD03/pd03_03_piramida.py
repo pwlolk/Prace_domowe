31f0009c771c5f03353ab125a5366d008bbe2bbb
pwlolk <p.j.olszak@gmail.com>
2018-11-25 08:53:01 +0100

    Wycinka prawie wszystkiego co nieudane

--- PD03/pd03_03_piramida.py
+++ PD03/pd03_03_piramida.py
@@ -11,32 +11,7 @@ while (row_current <= rows_total):
     row_current = row_current + 1
 print('')
 
-#PODEJŚCIA NIEUDANE (NIEDOKŁADNIE WPATRZYŁEM SIĘ W TREŚĆ ZADANIA)
-
-# rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
-# row_current = 1
-# while (row_current <= rows_total):
-#      print('{:^30}'.format(row_current*'#'))
-#      row_current = row_current + 1
-# print("")
-
-# rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
-# row_current = 1
-# while (row_current <= rows_total):
-#     print((row_current*'#').center(rows_total))
-#     row_current = row_current + 1
-# print("")
-
-# rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
-# row_current = 1
-# while (row_current <= rows_total):
-#     if (int(row_current)%2 == 1):
-#         print(' ' + (row_current*'#').center(rows_total))
-#     else:
-#         print((row_current*'#').center(rows_total))
-#     row_current = row_current + 1
-# print("")
-
+# Wersja "Krzywa choinka"
 # rows_total = int(input("Podaj wysokość piramidy [ilość poziomów]: "))
 # row_current = 1
 # while (row_current <= rows_total):
