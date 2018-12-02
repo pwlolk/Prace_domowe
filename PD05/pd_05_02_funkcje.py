9a351266b8ec1c06db5bc3a6bd2bd53df3e1349a
pwlolk <p.j.olszak@gmail.com>
2018-12-02 14:19:36 +0100

    Kolejne podejście do statystyk

--- PD05/pd_05_02_funkcje.py
+++ PD05/pd_05_02_funkcje.py
@@ -11,39 +11,40 @@ def letters(txt_file):
             number_of_letters = number_of_letters-5
     return number_of_letters
 
-# def sentences(txt_file):
-#     with open(txt_file, "a+") as file:
-#         file.seek(0)
-#         single_string = str(file.readlines())
-#     dots_spaces = 0
-#     for i,znaki in enumerate(single_string):
-#         if (single_string[i] == "." and single_string[i+1] == " "): # standardowe zakończenia zdań
-#             dots_spaces = dots_spaces+1
-#         if (single_string[i] == "." and single_string[i+1] == "'"): # koniec tekstu
-#             dots_spaces = dots_spaces+1
-#         if (single_string[i] == "." and single_string[i+1] == "\\" and single_string[i+2] == "n"): # breakline'y
-#             dots_spaces = dots_spaces+1
-#     print("Liczba zdań w tekście: " + str(dots_spaces))
-#
-# def words(txt_file):
-#     with open(txt_file, "a+") as file:
-#         file.seek(0)
-#         single_string = str(file.readlines())
-#     spaces = 0
-#     for i,znaki in enumerate(single_string):
-#         if (single_string[i] == " "):
-#             spaces = spaces+1
-#     print("Liczba słów w tekście: " + str(spaces+1))
+def sentences(txt_file):
+    with open(txt_file, "a+") as file:
+        file.seek(0)
+        single_string = str(file.readlines())
+    dots_spaces = 0
+    for i,znaki in enumerate(single_string):
+        if (single_string[i] == "." and single_string[i+1] == " "): # standardowe zakończenia zdań
+            dots_spaces = dots_spaces+1
+        if (single_string[i] == "." and single_string[i+1] == "'"): # koniec tekstu
+            dots_spaces = dots_spaces+1
+        if (single_string[i] == "." and single_string[i+1] == "\\" and single_string[i+2] == "n"): # breakline'y
+            dots_spaces = dots_spaces+1
+    return dots_spaces
 
-def characters_analyzed_counter(): #Wykonanie każdej statystyki powinno podbijać licznik, nawet jeżeli się powtarza
+def words(txt_file):
+    with open(txt_file, "a+") as file:
+        file.seek(0)
+        single_string = str(file.readlines())
+    spaces = 0
+    for i,znaki in enumerate(single_string):
+        if (single_string[i] == " "):
+            spaces = spaces+1
+    return spaces
+
+def characters_analyzed_counter(): #Wykonanie co najmniej jednej statystyki powinno podbijać licznik, nawet jeżeli się powtarza
     with open ("characters_analyzed.stat", "r+") as file:
         chars_count = int(file.read())
         chars_count = chars_count + int(letters(txt_file))
         file.seek(0)
         file.write(str(chars_count))
-        return(chars_count)
+        return(chars_count) # Zupełnie nie rozumiem co się dzieje z wartością zwróconą
 
+print("")
 print("Liczba znaków w pliku: " + str(letters(txt_file)))
-# sentences(txt_file)
-# words(txt_file)
+print("Liczba zdań w pliku: " + str(sentences(txt_file)))
+print("Liczba słów w pliku: " + str(words(txt_file)))
 print("Dotyczas przeanalizowano łącznie " + str(characters_analyzed_counter()) + " znaków.")
